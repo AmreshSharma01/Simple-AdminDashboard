037d7e47c2e5094a7f540083793ab48eec1cf65d
Amresh Sharma <amreshkrishna050@gmail.com>
2022-09-07 08:44:11 +0545

    modified:   src/components/Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import Card from './Card'
 import { FaAmazon } from 'react-icons/fa'
+import { FaFacebook, FaGoogle, FaMicrosoft } from 'react-icons/fa'
+
 
 const Cards = () => {
     return (
-        <div className='flex flex-row flex-wrap justify-center content-center ml-10 mt-5 '>
+        <div className='flex flex-row flex-wrap justify-center content-center  '>
             <div className='basis-1/2' >
                 <Card title="Product Added" numberCount={4444} bgColor='white' icons={<FaAmazon size='35' />} />
             </div>
             <div className='basis-1/2'>
-                <Card title="Product Added" numberCount={4444} bgColor='white' icons={<FaAmazon size='35' />} />
+                <Card title="Page Likes" numberCount={5000} bgColor='white' icons={<FaFacebook size='35' />} />
             </div>
             <div className="basis-1/2">
-                <Card title="Product Added" numberCount={4444} bgColor='white' icons={<FaAmazon size='35' />} />
+                <Card title="Google Photos" numberCount={6000} bgColor='white' icons={<FaGoogle size='35' />} />
             </div>
             <div className="basis-1/2">
-                <Card title="Product Added" numberCount={4444} bgColor='white' icons={<FaAmazon size='35' />} />
+                <Card title="Microsoft Products" numberCount={6040} bgColor='white' icons={<FaMicrosoft size='35' />} />
             </div>
         </div >
     )
