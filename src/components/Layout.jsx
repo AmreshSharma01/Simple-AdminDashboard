bb35bbcb15f2743d37bee1280a1f5249f74e9586
Amresh Sharma <amreshkrishna050@gmail.com>
2022-09-07 09:20:17 +0545

    modified:   src/components/Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Cards from './Cards'
-import SliderTest from './SliderTest'
+import Sidebar from './Sidebar'
 import LineCharts from './Charts/LineCharts'
 import TestChart from './Charts/TestChart'
 
@@ -14,19 +14,19 @@ import { data } from './datas/IotData'
 
 const Layout = () => {
     return (
-        <div className='relative'>
+        <div className='mx-60'>
 
-            <div className="grid grid-rows-6 grid-cols-5 gap-4 fixed w-full h-full"   >
-                <div className=' bg-white border-0 shadow-lg border-sky-500 row-start-1 row-span-6 col-span-1 ml-4 mt-4 rounded-lg'><h1 className='text-xl font-bold mb-4 ml-3'>DashBoard</h1> <SliderTest /></div>
-                <div className='mt-4 bg-white border-0 shadow-lg rounded-lg border-sky-500 row-start-1 row-span-2  col-start-2 col-span-4'> <Cards /></div>
-                <div className='bg-white border-0 shadow-lg rounded-lg border-sky-500 row-start-3 row-span-2 col-start-2 col-span-4 '  >
-                    <div className='max-w-2xl mt-3 ' >
+            <div className="grid grid-rows-6 grid-cols-5 gap-4"   >
+                <div className=' bg-white border-2 shadow-lg row-start-1 row-span-6 col-span-1 ml-4 mt-4 rounded-lg'><h1 className='text-xl font-bold pt-2 mb-4 ml-3'>DashBoard</h1> <Sidebar /></div>
+                <div className='mt-4 mb-3 bg-white border-2 shadow-lg rounded-lg row-start-1 row-span-2  col-start-2 col-span-4'> <Cards /></div>
+                <div className='pt-1 bg-white border-2 shadow-lg rounded-lg row-start-3 row-span-2 col-start-2 col-span-4 transition ease-in-out hover:bg-gray-100 hover:-translate-y-3 duration-200 '  >
+                    <div className='max-w-2xl mt-3 m-auto' >
                         <LineCharts data={data} width={500} height={400} />
                     </div>
                 </div>
 
-                <div className='mt-3 mb-3 bg-white border-0  border-sky-500 row-start-5 row-span-2 col-start-2 col-span-4 rounded-lg shadow-lg'>
-                    <div className='max-w-2xl '>
+                <div className='mt-3 mb-3 pt-3 bg-white border-2  row-start-5 row-span-2 col-start-2 col-span-4 rounded-lg shadow-lg transition ease-in-out hover:bg-gray-100 hover:-translate-y-3 duration-200'>
+                    <div className='max-w-2xl m-auto '>
                         <TestChart />
                     </div>
                 </div>
