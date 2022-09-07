a59ed2233b7f565bdc15fd84425cd1d125322a20
Amresh Sharma <amreshkrishna050@gmail.com>
2022-09-07 09:33:14 +0545

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,11 @@ import React from 'react'
 // import Dashboard from './components/Dashboard'
 import TestChart from './components/Charts/TestChart'
 import Layout from './components/Layout'
-import SliderTest from './components/SliderTest'
+import Sidebar from './components/Sidebar'
 const App = () => {
   return (
-    <div className='container-auto'>
-      <h1 className='text-3xl text-center font-bold'>Admin Dashboard</h1>
-      {/* <h1 className='text-3xl font-bold underline'>Dashboard</h1> */}
-      {/* <Dashboard />
-      <TestChart /> */}
-
+    <div >
       <Layout />
-      {/* <SliderTest /> */}
-
     </div>
   )
 }
